6d0b729d3122594ba21c52cd67ffe0701bd029af
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2025-06-06 20:15:07 +0300

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component in ClientLayout

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -3,15 +3,18 @@
 import Navbar from '../components/navbar';
 import { Analytics } from '@vercel/analytics/next';
 import { SessionProvider } from 'next-auth/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
+      
       <div id="navbar">
         <Navbar />
       </div>
       <div id="main">{children}</div>
       <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   );
 }
