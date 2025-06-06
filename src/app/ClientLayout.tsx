56fab894562004b4ebf6f7f44bec7a7066914d84
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2025-06-06 20:18:05 +0300

    fix: fixed "Error: Replace `"@vercel/speed-insights/next"` with `'@vercel/speed-insights/next';`  prettier/prettier" error
    - linted code

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -3,12 +3,11 @@
 import Navbar from '../components/navbar';
 import { Analytics } from '@vercel/analytics/next';
 import { SessionProvider } from 'next-auth/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      
       <div id="navbar">
         <Navbar />
       </div>
